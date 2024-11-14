f3d76fcd47281934afaa0cbbbd94b07474a5ac28
nekosu <liao.hy@outlook.com>
2024-11-14 20:58:20 +0800

    chore: temp adding vfctprintf

--- printf.h
+++ printf.h
@@ -108,6 +108,7 @@ int vprintf_(const char* format, va_list va);
  */
 int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
 
+int vfctprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va);
 
 #ifdef __cplusplus
 }
